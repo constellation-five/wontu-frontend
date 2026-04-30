0fa0c9a8b248ab2ad89fc853472884d0d037d2bf
NgurahRadithya <ngurahbagus13@gmail.com>
2026-04-30 15:24:05 +0700

    feat: payment-method

--- src/app/features/profile/profile-page.ts
+++ src/app/features/profile/profile-page.ts
@@ -1,7 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'profile-page',
+  imports: [MatButtonModule, RouterModule],
   templateUrl: './profile-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
