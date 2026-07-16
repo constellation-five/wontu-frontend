46539f8748a2c198df1d950cb69651cf0ebc4064
claudiahann <claudia.mamahit@binus.ac.id>
2026-07-16 19:49:22 +0700

    refactor: clean and complete some code

--- src/app/features/request/request-card/request-card.ts
+++ src/app/features/request/request-card/request-card.ts
@@ -10,13 +10,21 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CategoryLabelComponent } from '../../../shared/components/category-label/category-label';
+import { IconButtonVariantDirective } from '../../../shared/directives/button';
 
 @Component({
   selector: 'app-request-card',
   templateUrl: './request-card.html',
   styleUrls: ['./request-card.scss'],
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, CategoryLabelComponent],
+  imports: [
+    CommonModule, 
+    MatCardModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    CategoryLabelComponent,
+    IconButtonVariantDirective
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RequestCard {
