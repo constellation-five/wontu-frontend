53cec0c86f3d4774e95d8e71fd35436ede45aaee
claudiahann <claudia.mamahit@binus.ac.id>
2026-07-20 17:11:02 +0700

    feat: create activity card

---
+++ src/app/features/activity/activity-card/activity-card.ts
@@ -0,0 +1,39 @@
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { CategoryLabelComponent } from '../../../shared/components/category-label/category-label';
+import { RupiahFormatPipe } from '../../../shared/pipes/rupiah-format.pipe';
+
+@Component({
+  selector: 'app-activity-card',
+  templateUrl: './activity-card.html',
+  styleUrls: ['./activity-card.scss'],
+  standalone: true,
+  imports: [MatCardModule, MatIconModule, CategoryLabelComponent, RupiahFormatPipe],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ActivityCardComponent {
+  type = input.required<'Order' | 'Offer'>();
+  merchantName = input.required<string>();
+  locationLabel = input.required<string>();
+  date = input.required<string>();
+  statusText = input.required<string>();
+  statusColor = input.required<string>();
+  imageUrl = input<string>('');
+  totalPrice = input<number>(0);
+  isHistory = input<boolean>(false);
+  
+  rateClick = output<void>();
+
+
+  cardClick = output<void>();
+
+  onCardClick() {
+    this.cardClick.emit();
+  }
+  
+  onRateClick(event: Event) {
+    event.stopPropagation();
+    this.rateClick.emit();
+  }
+}
